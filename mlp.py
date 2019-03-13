a18f29419790a56c375586291a89c3b87d235923
Anthony Dickson <eight0153@gmail.com>
2019-03-14 08:52:45 +1300

    implement minibatch sgd and add a few useful methods

--- mlp.py
+++ mlp.py
@@ -1,6 +1,34 @@
 import numpy as np
 
 
+def _generate_minibatches(X, y, batch_size=32):
+    N = len(X)
+    assert N == len(y)
+
+    if batch_size == 1:  # standard sgd
+        batch_size = 1
+        n_batches = N
+        last_batch_size = batch_size
+    elif batch_size == -1:  # batch sgd
+        batch_size = N
+        n_batches = 1
+        last_batch_size = batch_size
+    else:  # minibatch sgd
+        n_batches, last_batch_size = divmod(N, batch_size)
+        n_batches = n_batches + (1 if last_batch_size > 0 else 0)
+
+    ix = 0
+
+    for batch_i in range(n_batches):
+        batch_start = ix
+        batch_end = ix + batch_size
+
+        if batch_end > N:
+            batch_end = ix + last_batch_size
+
+        yield batch_i, X[batch_start:batch_end], y[batch_start:batch_end]
+
+
 class MLP:
     def __init__(self, n_inputs, n_hidden, n_outputs, learning_rate):
         self.learning_rate = learning_rate
@@ -18,31 +46,30 @@ class MLP:
         self.output_activation = None
 
     @staticmethod
-    def sigmoid(x):
-        z = np.exp(x)
+    def sigmoid(X):
+        Z = np.exp(X)
 
-        return z / (z + 1)
+        return Z / (Z + 1)
 
     @staticmethod
-    def dsigmoid(y):
-        return y * (1 - y)
+    def dsigmoid(Y):
+        return Y * (1 - Y)
 
-    def forward(self, x):
-        self.prev_input = x
+    def forward(self, X):
+        self.prev_input = X
 
-        output = self.W_0.dot(x) + self.b_0
+        output = X.dot(self.W_0) + self.b_0
         output = MLP.sigmoid(output)
         self.hidden_activation = output
 
-        output = self.W_1.dot(output) + self.b_1
+        output = output.dot(self.W_1.T) + self.b_1
         output = MLP.sigmoid(output)
         self.output_activation = output
 
         return output
 
-    def backward(self, y, y_pred):
-        error = y - y_pred
-        output_error = error * MLP.dsigmoid(self.output_activation)
+    def backward(self, errors):
+        output_error = errors * MLP.dsigmoid(self.output_activation)
         hidden_error = output_error.dot(self.W_1) * MLP.dsigmoid(self.hidden_activation)
 
         dW_1 = self.learning_rate * output_error * self.hidden_activation
@@ -50,24 +77,32 @@ class MLP:
         dW_0 = self.learning_rate * hidden_error * self.prev_input
         db_0 = self.learning_rate * hidden_error
 
-        self.W_1 += dW_1
-        self.b_1 += db_1
-        self.W_0 += dW_0
-        self.b_0 += db_0
+        self.W_1 += dW_1.mean(axis=0)
+        self.b_1 += db_1.mean(axis=0)
+        self.W_0 += dW_0.mean(axis=0)
+        self.b_0 += db_0.mean(axis=0)
 
-    def fit(self, X, y, n_epochs):
+    def fit(self, X, y, n_epochs=100, batch_size=-1):
         for epoch in range(n_epochs):
-            print('Epoch %d of %d' % (epoch + 1, n_epochs))
-            errors = []
+            epoch_error_history = np.array([])
 
-            for instance, target in zip(X, y):
-                target_pred = self.forward(instance)
-                error = target - target_pred
-                errors.append(error)
-                print('Pattern: %s - Target: %d - Predicted Target: %.4f' % (
-                    instance, target, target_pred))
-                self.backward(target, target_pred)
+            for _, X_batch, y_batch in _generate_minibatches(X, y, batch_size):
+                target_pred = self.forward(X_batch)
+                errors = y_batch - target_pred
+                self.backward(errors)
+                epoch_error_history = np.append(epoch_error_history, errors)
 
-            rmse = np.sqrt(np.mean(np.square(errors)))
+            rmse = np.sqrt(np.mean(np.square(epoch_error_history)))
 
-            print('RMSE: %.4f' % rmse)
+            print('Epoch %d of %d - RMSE: %.4f' % (epoch + 1, n_epochs, rmse))
+
+    def predict_proba(self, X):
+        return self.forward(X)
+
+    def predict(self, X):
+        return np.argmax(self.predict_proba(X), axis=0)
+
+    def score(self, X, y):
+        y_pred = self.predict_proba(X)
+
+        return np.sqrt(np.mean(np.square(y - y_pred)))
--- xor.py
+++ xor.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.utils import shuffle
 
 from datasets import load_XOR
 from mlp import MLP
@@ -7,6 +8,8 @@ if __name__ == '__main__':
     np.random.seed(42)
 
     X, y = load_XOR()
-    mlp = MLP(X.shape[1], 2, 1, 1)
+    X, y = shuffle(X, y, random_state=42)
+    mlp = MLP(X.shape[1], 2, 1, 2)
 
-    mlp.fit(X, y, n_epochs=1000)
+    mlp.fit(X, y, n_epochs=2000, batch_size=4)
+    print("Score: %.4f (Lower is better)" % mlp.score(X, y))
